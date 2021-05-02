1e2fa74667395fdb90ad927162468e39165f74ba
ziaur-rahman44 <ziaurfahim44@gmail.com>
2021-05-02 21:27:49 +0600

    some change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import 'bootstrap/dist/js/bootstrap.bundle';
